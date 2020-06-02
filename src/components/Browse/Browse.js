6b17e67be35d6f2d8f2b2a3ffb352b8d472067b7
Sarah Park <parks96uw@users.noreply.github.com>
2020-06-01 20:01:06 -0700

    Bug fix routing (#9)
    * added redux and restructured components
    
    * unused variable

--- src/components/Browse/Browse.js
+++ src/components/Browse/Browse.js
@@ -1,38 +1,68 @@
 import React from 'react';
-// import Pagination from "react-js-pagination";
 
-import CardList from '../CartList/CardList';
+import { connect } from 'react-redux';
+import { fetchPagedPokemon, clearPagedPokemon } from '../../actions';
+
+import Card from '../Card/Card';
 import browse from '../images/browseDex.png';
 import './Browse.css';
 
-const Browse = (props) => {
-    return (
-        <div className="browse ui container">
-            <div className="browse-header">
-                <div className="browse-image-container">
-                    <img className="browse-image" src={browse} alt={"pokemon-search"} />
+class Browse extends React.Component {
+    state = {
+        itemsPerPage: 20
+    }
+
+    handlePageChange = async (page) => {
+        this.props.fetchPagedPokemon(page);
+    }
+
+    // Grab the first page of pokemon
+    componentDidMount() {
+        this.props.fetchPagedPokemon(this.props.page);
+    }
+
+    // Clear the list of items
+    componentWillUnmount() {
+        this.props.clearPagedPokemon();
+    }
+
+    renderedList() {
+        if (!this.props.list) return null;
+        return this.props.list.map(item => {
+            const id = item.url.match(/(?:\/)([0-9]+)/).toString().split(',')[1];
+            return (
+                <div className="four wide column" key={item.name}>
+                    <Card data={item} id={id} />
                 </div>
+            )
+        });
+    }
+
+    render() {
+        return (
+            <div className="browse ui container">
+                <div className="browse-header">
+                    <div className="browse-image-container">
+                        <img className="browse-image" src={browse} alt={"pokemon-search"} />
+                    </div>
+                </div>
+                <div className="cardlist-segment ui segment">
+                    <div className="ui stackable grid">{this.renderedList()}</div>
+                </div>
+                {this.props.list && this.props.count > this.props.list.length && <button className="browse-load-btn ui button"
+                    onClick={() => this.handlePageChange(this.props.page + 1)}>Load More</button>}
             </div>
-            <CardList results={props.results} />
-            {props.totalItemsCount > props.results.length && <button
-                className="browse-load-btn ui button"
-                onClick={() => props.onChange(props.activePage + 1)}>
-                Load More
-            </button>}
-            {/* <Pagination
-                className="pagination"
-                activePage={activePage}
-                itemsCountPerPage={itemsCountPerPage}
-                totalItemsCount={totalItemsCount}
-                pageRangeDisplayed={5}
-                itemClass="page-item"
-                linkClass="page-link"
-                hideDisabled={true}
-                hideFirstLastPages={true}
-                onChange={onChange}
-            /> */}
-        </div>
-    )
+        )
+
+    }
 }
 
-export default Browse;
+const mapStateToProps = (state) => {
+    return {
+        list: state.results.list,
+        count: state.results.count,
+        page: state.results.page
+    };
+}
+
+export default connect(mapStateToProps, { fetchPagedPokemon, clearPagedPokemon })(Browse);
